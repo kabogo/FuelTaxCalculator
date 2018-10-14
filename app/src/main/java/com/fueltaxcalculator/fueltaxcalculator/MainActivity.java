3ac04867d26bfd3cd3c360856bc4db1b77577699
munene <jeremymunene@gmail.com>
2018-10-14 21:58:56 +0300

    - First

---
+++ app/src/main/java/com/fueltaxcalculator/fueltaxcalculator/MainActivity.java
@@ -0,0 +1,97 @@
+package com.fueltaxcalculator.fueltaxcalculator;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fxn769.Numpad;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.amountTextView)
+    TextView amountTextView;
+
+    @BindView(R.id.numpad)
+    Numpad numpad;
+
+    @BindView(R.id.fuelTypeRadioGroup)
+    RadioGroup radioGroup;
+
+    private final static String NAME = "Settings";
+    String fuelType = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        ((RadioButton)radioGroup.getChildAt(0)).setChecked(true);
+
+        numpad.setOnTextChangeListner((String text, int digits_remaining) -> {
+            amountTextView.setText(text);
+        });
+
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            for (int i = 0; i < 3; i++){
+                RadioButton radioButton = ((RadioButton)radioGroup.getChildAt(i));
+                if (radioButton.isChecked()){
+                    fuelType = radioButton.getTag().toString();
+                }
+            }
+        });
+    }
+
+    @OnClick(R.id.calculate_button)
+    public void calculate(View view) {
+        if (TextUtils.isEmpty(fuelType))return;
+
+        String price = getSharedPrefs().getString(fuelType + "_price", null);
+
+        String levy = getSharedPrefs().getString(fuelType + "_levy", null);
+
+        String vat = getSharedPrefs().getString(fuelType + "_vat", null);
+
+        if (price == null || levy == null || vat == null){
+            SettingsDialog settingsDialog = new SettingsDialog(this, false, dialogInterface -> {
+                //Do nothing
+            });
+
+            settingsDialog.show();
+
+            Toast.makeText(this, "You need to enter some settings first", Toast.LENGTH_LONG).show();
+        }else {
+            try{
+                double decimalPrice = Double.parseDouble(price);
+                double decimalLevy = Double.parseDouble(levy);
+                double decimalVat = Double.parseDouble(vat);
+
+                double tax = (decimalPrice - decimalLevy) * (decimalVat/100);
+                String readableTax = String.valueOf(tax);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(readableTax)
+                        .setTitle("Your Tax value");
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }catch (Exception ex){
+                Toast.makeText(this, "We were unable to calculate your tax. Please check your figures and try again", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private SharedPreferences getSharedPrefs() {
+        return this.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+    }
+}
---
+++ app/src/main/java/com/fueltaxcalculator/fueltaxcalculator/SettingsDialog.java
@@ -0,0 +1,81 @@
+package com.fueltaxcalculator.fueltaxcalculator;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by munene on 10/14/2018.
+ */
+public class SettingsDialog extends Dialog {
+    @BindView(R.id.titleTextView)
+    TextView titleTextView;
+
+    @BindView(R.id.priceEditText)
+    EditText priceEditText;
+
+    @BindView(R.id.levyEditText)
+    EditText levyEditText;
+
+    @BindView(R.id.vatEditText)
+    EditText vatEditText;
+
+    @BindView(R.id.saveButton)
+    Button saveButton;
+
+    private final static String NAME = "Settings";
+
+    protected SettingsDialog(@androidx.annotation.NonNull Context context, boolean cancelable, @androidx.annotation.Nullable DialogInterface.OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_settings);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.saveButton)
+    public void save(View view) {
+        if (TextUtils.isEmpty(priceEditText.getText().toString()))
+        {
+            Toast.makeText(this.getContext(), "You need to set the price", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(levyEditText.getText().toString()))
+        {
+            Toast.makeText(this.getContext(), "You need to set the levy", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(vatEditText.getText().toString()))
+        {
+            Toast.makeText(this.getContext(), "You need to set the VAT", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        getSharedPrefs().edit().putString(titleTextView + "_price", priceEditText.getText().toString()).apply();
+
+        getSharedPrefs().edit().putString(titleTextView + "_levy", levyEditText.getText().toString()).apply();
+
+        getSharedPrefs().edit().putString(titleTextView + "_vat", vatEditText.getText().toString()).apply();
+
+        this.hide();
+    }
+
+    private SharedPreferences getSharedPrefs() {
+        return this.getContext().getSharedPreferences(NAME, Context.MODE_PRIVATE);
+    }
+}
